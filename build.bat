43f23289edc0c98b4141ebf726434ffdc46409c1
zhangyi <zh1@vip.qq.com>
2024-01-16 10:47:02 +0800

    1

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 cd ./proto
 protoc --proto_path=./ --proto_path=../  --go_out=../../ *.proto
-protoc --proto_path=./ --proto_path=../  --go-grpc_out=../../ table.proto
-protoc --proto_path=./ --proto_path=../  --go-grpc_out=../../ tableCluster.proto
+protoc --proto_path=./ --proto_path=../  --go-grpc_out=../../ tableMonitor.proto
+protoc --proto_path=./ --proto_path=../  --go-grpc_out=../../ tableClusterMonitor.proto
 
 cd ../pb/control
 protoc-go-inject-tag -input="./control.pb.go"
